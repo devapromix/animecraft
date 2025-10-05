4d90bb9e525e6632e6a1b780bc2a509107047a14
devapromix <maxwof@ukr.net>
2025-10-05 11:16:27 +0300

    Повна анімація руху Стіва.

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ local time = 0
 local speed = 4  -- Швидкість коливання (радіани за секунду)
 local leg_amp = 0.5  -- Амплітуда для ніг (радіани, ~28 градусів)
 local arm_amp = 0.3  -- Амплітуда для рук (менша, для природності)
+local joint_offset = 3  -- X-offset для точки кріплення (верхня середина спрайту, припускаючи ширину ~6 пікселів)
 
 function love.load(args)
 	math.randomseed(os.time())
@@ -20,18 +21,22 @@ end
 
 function d(x, y, s)
 	local center_x = x + (3 * s)  -- Центр тіла для зручності
+	local leg_y = y + (42 * s) - 10
+	local arm_y = y + (18 * s) + 10
+	local arm_left = 30
+	local leg_left = 30
 	
-	-- Ноги: розділив позиції, щоб не накладалися (ліва - зліва, права - справа)
-	love.graphics.draw(leg1, center_x - (3 * s), y + (42 * s), v, s)
-	love.graphics.draw(leg2, center_x + (3 * s), y + (42 * s), h, s)
+	-- Найдальший шар: ноги та задня рука (arm1)
+	love.graphics.draw(leg1, center_x + leg_left, leg_y, v, -s, s, joint_offset, 0)
+	love.graphics.draw(leg2, center_x + leg_left, leg_y, h, s, s, joint_offset, 0)
+	love.graphics.draw(arm1, center_x + arm_left, arm_y, arm_v, -s, s, joint_offset, 0)
 	
-	-- Руки: аналогічно, на рівні тіла, протилежний рух до ніг
-	love.graphics.draw(arm1, center_x - (3 * s), y + (18 * s), arm_v, s)
-	love.graphics.draw(arm2, center_x + (3 * s), y + (18 * s), arm_h, s)
-	
-	-- Тіло та голова (зсунув голову під центр)
+	-- Середній шар: тулуб та голова
 	love.graphics.draw(body, center_x, y + (18 * s), 0, s)
 	love.graphics.draw(head, center_x - (3 * s), y, 0, s)
+	
+	-- Найближчий шар: передня рука (arm2)
+	love.graphics.draw(arm2, center_x + arm_left, arm_y, arm_h, s, s, joint_offset, 0)
 end
 
 function love.draw()
